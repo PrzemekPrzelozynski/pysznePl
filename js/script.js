1922a35565c5d4210dc821b443ecb7bc4d38b8a0
PrzemekPrzelozynski <przelozynskip@gmail.com>
2020-09-20 09:40:14 +0100

    cwiczenia DOM2

--- js/script.js
+++ js/script.js
@@ -3,10 +3,5 @@ const mainBnt = document.getElementById('btnId');
 mainBnt.addEventListener("click", takeOrder);
 
 function takeOrder () {
-    var pomidorowy = document.getElementById('pomidorowy');
-    console.log(pomidorowy.checked);
-    var czosnkowy = document.getElementById('czosnkowy');
-    console.log(czosnkowy.checked);
-    var ostryPomidorowy = document.getElementById('ostryPomidorowy');
-    console.log(ostryPomidorowy.checked);
+
 }
